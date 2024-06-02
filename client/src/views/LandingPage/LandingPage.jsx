9801ce1d687643faf702d97e8c6fcb5b6465ca5c
Victor Sepulveda <victorseva123@gmail.com>
2024-06-01 20:02:23 -0500

    Landingpage

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -71,7 +71,9 @@ const LandingPage = () => {
       <div className={styles.landingPageOverlay}></div>
       <div className={styles.content}>
         <div>
-        <h1>Bienvenido a Propiedades & Diseño</h1>
+        <h1>Bienvenidos</h1>
+        <h1>A</h1>
+        <h1>Programando sueños</h1>
         <Link to="/home" className={styles.button}>Iniciar como Invitado</Link>
         </div>
         <div>-</div>
